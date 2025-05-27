76680d504ef7a2cf813016a99c0371117986456a
deeperthought <deeperthought@noreply.gitcode.com>
2025-05-27 20:39:50 +0800

    refactor(rtlsdr_setup): 调整接收机配置参数
    - 修改中心频率为 879.4 MHz
    - 禁用自动增益控制 (AGC)
    - 设置固定增益为 80 dB

--- rtlsdr_setup.m
+++ rtlsdr_setup.m
@@ -16,14 +16,16 @@ function sdrobj = rtlsdr_setup()
     sdrobj.SampleRate = 1.92e6;
 
     % 设置中心频率（959.8MHz）
-    sdrobj.CenterFrequency = 959.8e6;
+    sdrobj.CenterFrequency = 879.4e6;
+        % sdrobj.CenterFrequency = 959.8e6;
+
 
     % 设置频率校正因子（ppm）
     sdrobj.FrequencyCorrection = 55;  % 设备频偏为55ppm
 
     % 设置增益模式和增益
-    sdrobj.EnableTunerAGC = true;
-    % sdrobj.TunerGain = 80;  % 可根据实际情况调整
+    sdrobj.EnableTunerAGC = false;
+    sdrobj.TunerGain = 80;  % 可根据实际情况调整
 
     % 设置帧长度
     % NB-IoT子帧长度为10ms，对应于1.92MHz采样率下的19200个样本
